4fba521894ff9e7eb6d4ba4646d661a1c8cc0ecd
Trailblazer-dev <richvictor830@gmail.com>
2024-08-28 11:43:30 +0300

    building

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Navbar from "./components/Navbar"
 import HeroSection from "./components/HeroSection"
+import FeatureSection from "./components/FeatureSection"
+import Workflow from "./components/Workflow"
 function App() {
   return (
     <>
       <Navbar></Navbar> 
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection/>
+        <FeatureSection/>
+        <Workflow/>        
       </div>
       
     </>
